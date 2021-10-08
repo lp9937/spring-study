1b8512379fee055232f256caaa81ceb9c9c827a0
lp9937 <lp9937@sina.com>
2021-10-08 17:40:01 +0800

    spring boot 服务注册

---
+++ study-eureka-server/src/main/java/com/study/eureka/security/config/SecurityConfig.java
@@ -0,0 +1,20 @@
+package com.study.eureka.security.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @author lp
+ * @description TODO
+ * @date 2021/10/8 16:04
+ */
+@EnableWebSecurity
+public class SecurityConfig extends
+        WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().ignoringAntMatchers("/eureka/**");
+        super.configure(http);
+    }
+}
